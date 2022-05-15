b9729ffc7618735aacc0ce7bb63eaa9103ff07af
inayat13 <104351725+inayat13@users.noreply.github.com>
2022-05-15 22:38:04 +0530

    Add files via upload

---
+++ Assignment 2/Python 2 Output.py
@@ -0,0 +1,95 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================== RESTART: D:\Pec\Python\Python 2.py ======================
+Ques.1}
+
+
+Python is a case sensitive language
+
+a. Length of the entered string is: 35 
+
+b. Reversed Order of String: egaugnal evitisnes esac a si nohtyP 
+
+c. New String is: a case sensitive 
+
+d. Repalced String is: Python is object oriented language 
+
+e. Index of Substring 'a' is: 10 
+
+Your String with no spaces Pythonisacasesensitivelanguage
+
+
+
+
+
+Ques.2}
+
+
+Enter Your Name: Inayat Kaur Shah
+Enter Your SID: 21102028
+Enter Your Department: Civil
+Enter Your CGPA: 8.5
+Hey, Inayat Kaur Shah Here!
+My SID is 21102028
+I am from Civil Department and My CGPA is 8.500000
+
+
+
+
+
+Ques.3}
+
+
+a =  56 
+ b =  10
+a. a&b =  8 
+
+b. a|b =  58 
+
+c. a^b =  50 
+
+d. Left Shift of 'a' by 2 bits =  224 
+   Left Shift of 'b' by 2 bits =  40
+e. Right Shift of 'a' by 2 bits =  14 
+   Right Shift of 'b' by 4 bits =  0
+
+
+
+
+
+Ques.4}
+
+
+Enter a sentence to know wheather it contain 'name'or not:-
+
+Enter Your Sentence: My name is Inayat
+
+ Does this sentence contain word 'name' : Yes
+
+
+
+
+
+Ques.5}
+
+
+Enter three lengths to se wheather a triangle can be formed from these lenghts or not:-
+
+Length of First Side of Triangle = 13
+Length of Second Side of Trisngle = 6
+Length of Third Side of Triangle = 25
+
+No, Triangle can't be formed.
+
+
+
+
+
+Ques.6}
+
+
+Enter 1st number 5
+Enter 1st number 10
+4
+0b1111
