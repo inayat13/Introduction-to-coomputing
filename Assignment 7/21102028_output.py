09ee25334f8730c7b3a73d38b609f1dbed34aff0
inayat13 <104351725+inayat13@users.noreply.github.com>
2022-07-16 10:47:32 -0700

    Add files via upload

---
+++ Assignment 7/21102028_output.py
@@ -0,0 +1,80 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: E:/Pec/Python/Assignment 7/21102028_source.py ============
+Q1
+
+
+
+
+
+
+
+Q2
+
+
+
+
+
+
+
+Q3
+
+
+
+
+
+
+
+Q4
+
+
+Enter size of list: 8
+Enter marks: 12
+Enter marks: 19
+Enter marks: 15
+Enter marks: 18
+Enter marks: 3
+Enter marks: 18
+Enter marks: 1
+Enter marks: 20
+[1, 3, 12, 15, 18, 18, 19, 20]
+
+
+
+
+
+Q5
+
+
+Enter size of array: 8
+Enter element 45
+Enter element 56
+Enter element 48
+Enter element 15
+Enter element 36
+Enter element 85
+Enter element 14
+Enter element 69
+[14, 15, 36, 45, 48, 56, 69, 85]
+Enter element to be searched and counted: 6
+Error! Element is not present.
+Number of occurrences of element is 0
+
+
+
+
+
+Q6
+
+
+Enter number of inputs:8
+Enter number:5
+Enter number:4
+Enter number:6
+Enter number:9
+Enter number:8
+Enter number:1
+Enter number:3
+Enter number:7
+[1, 3, 4, 5, 6, 7, 8, 9]
